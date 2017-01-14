af58f186cae768dd72db0304ce25e1811fab287b
Aakash Sasikumar <aakash3697@gmail.com>
2017-01-14 13:04:44 +0530

    added gui and video capture

--- SRC/Java/Main.java
+++ SRC/Java/Main.java
@@ -1,1 +1,31 @@
+import org.opencv.core.Core;
 
+import javax.swing.*;
+
+/**
+ * Created by Aakash on 1/14/2017.
+ */
+public class Main {
+    static{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new MainFrame();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedLookAndFeelException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
