4985546feaf9b07b1e3d77257290e26606021d19
Aakash Sasikumar <aakash3697@gmail.com>
2017-02-26 15:55:13 +0530

    Update VolumeScript.ps1

--- SRC/Java/VolumeScript.ps1
+++ SRC/Java/VolumeScript.ps1
@@ -1,8 +1,8 @@
-cd $PSScriptRoot
+cd $PSScriptRoot#it changes the directory to the location of the script
 while($true){
-$checker = Test-Path check.txt
+$checker = Test-Path check.txt#check to see if a file named check.txt exists
 if($checker -eq $true){
-Set-Content .\NirVolume.txt "$null"
+Set-Content .\NirVolume.txt "$null"#it will exit if so
 break
 }
 $var = cat .\NirVolume.txt
@@ -11,6 +11,6 @@ continue
 }
 $vol=$var[$var.Length-2]
 $vol
-.\nircmd.exe setvolume 0 $vol $vol
+.\nircmd.exe setvolume 0 $vol $vol #the command to set the volume
 }
 exit
